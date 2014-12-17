db8d8c4621886a3238c949b8abf7c3ad45a2b144
Laser Nite <lasernite@gmail.com>
2014-12-17 00:18:02 -0500

    building posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def new
   end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	@post.save
+  	redirect_to @post
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def index
+  	@post = Post.all
+  end
+
+  private
+    def post_params
+  	  params.require(:post).permit(:text)
+    end
+
 end
